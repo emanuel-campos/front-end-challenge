ecc2cfacd17a91c4c65db0e46ed4f03aceb9f23a
Emanuel Campos <emanuelcampos.contato@gmail.com>
2018-11-12 15:10:01 -0300

    criar o container Calls da Home

---
+++ src/app/screens/Home/components/containers/Calls/Calls.data.js
@@ -0,0 +1,19 @@
+const MockCalls = [
+  {
+    title: 'Imperdível',
+    subtitle: 'Taxa Zero %',
+    description: 'Always first off the line. Instant torque delivers full power in a heartbeat.',
+  },
+  {
+    title: 'O carro mais',
+    subtitle: 'Seguro',
+    description: 'Always first off the line. Instant torque.',
+  },
+  {
+    title: 'Bonús de até',
+    subtitle: 'R$ 3 mil',
+    description: 'Always first off the line. Instant torque delivers full power in a heartbeat.',
+  },
+];
+
+export default MockCalls;
---
+++ src/app/screens/Home/components/containers/Calls/Calls.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+  Grid,
+  Row,
+  Col,
+} from 'react-bootstrap';
+
+import CallsItem from '../../presentation/CallsItem/CallsItem';
+
+// mock de dados usados no container Calls
+import MockCalls from './Calls.data';
+
+// estilo Sass para o componente
+import './Calls.style.scss';
+
+/**
+ * Calls é um componente do tipo container
+ * que carrega um conjunto de chamadas.
+ */
+
+class Calls extends Component {
+  render() {
+    return (
+      <div className="calls">
+        <Grid>
+          <Row>
+            {MockCalls.map((call, index) => (
+              <Col className="calls-item" xs={12} md={4} key={index.toString()}>
+                <CallsItem
+                  title={call.title}
+                  subtitle={call.subtitle}
+                  description={call.description}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Calls;
