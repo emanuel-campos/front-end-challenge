f4b34e39f24cf7d267485deb5cd6659021289471
Emanuel Campos <emanuelcampos.contato@gmail.com>
2018-11-12 00:58:32 -0300

    corrigir organizacao do codigo

--- src/app/components/presentation/DropdownCars/DropdownCars.test.js
+++ src/app/components/presentation/DropdownCars/DropdownCars.test.js
@@ -6,23 +6,35 @@ import DropdownCars from './DropdownCars';
 /**
  * Lista com o resumo dos testes abaixo:
  *
- * 1 - Renderizar o componente sem erros
- * 2 - Listar todos os veiculos recebidos pela prop
- * 3 - Carregar uma tag img para o icone do carro
- * 4 - A prop img src deve receber o valor da url/path passada
- * 5 - Não deve renderizar o img se o icone do carro não for passado
- * 6 - Mostrar texto com o nome dos veiculos na lista
+ * 01 - Renderizar o componente sem erros
+ * 02 - Listar todos os veiculos recebidos pela prop
+ * 03 - Carregar uma tag img para o icone do carro
+ * 04 - A prop img src deve receber o valor da url/path passada
+ * 05 - Não deve renderizar o img se o icone do carro não for passado
+ * 06 - Mostrar texto com o nome dos veiculos na lista
  */
 
 describe('<DropdownCars />', () => {
     const cars = [
-      { name: 'Etios Sedã 2017', icon: 'local/to/path/img' },
-      { name: 'Etios Sedã 2017', icon: 'local/to/path/img' },
-      { name: 'Etios Sedã 2017', icon: 'local/to/path/img' },
-      { name: 'Etios Sedã 2017' },
-      { name: 'Etios Sedã 2017', icon: 'local/to/path/img' },
-      { name: 'Etios Sedã 2017', icon: 'local/to/path/img' },
-      { name: 'Etios Sedã 2017', icon: 'local/to/path/img' },
+      {
+        name: 'Nome do carro',
+        icon: 'local/to/path/img'
+      },
+      {
+        name: 'Novo titulo do carro',
+        icon: 'local/to/path/img'
+      },
+      {
+        name: 'Mais nome do carro',
+        icon: 'local/to/path/img'
+      },
+      {
+        name: 'Esse nome do carro'
+      },
+      {
+        name: 'Nome do carro 2017',
+        icon: 'local/to/path/img'
+      },
     ];
 
     // 1 - Renderizar o componente sem erros
