c3a85e9434c38fd5a7a21e9f79c0c164f2da348c
Emanuel Campos <emanuelcampos.contato@gmail.com>
2018-11-14 16:45:42 -0300

    adicionar container Footer na screen Home

--- src/app/screens/Home/Home.js
+++ src/app/screens/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from '@/app/components/containers/Header/Header';
+import Footer from '@/app/components/containers/Footer/Footer';
 import Showcase from './components/containers/Showcase/Showcase';
 import Calls from './components/containers/Calls/Calls';
 import Versions from './components/containers/Versions/Versions';
@@ -20,6 +21,7 @@ class Home extends Component {
         <Calls />
         <Versions />
         <Cars />
+        <Footer />
       </div>
     );
   }
