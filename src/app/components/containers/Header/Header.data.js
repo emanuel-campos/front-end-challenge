7724b772fd11fa957defa0d119250123d523d40e
Emanuel Campos <emanuelcampos.contato@gmail.com>
2018-11-15 00:57:13 -0300

    adicionar conteudo no menu Nossos Veiculos do Header

--- src/app/components/containers/Header/Header.data.js
+++ src/app/components/containers/Header/Header.data.js
@@ -3,6 +3,10 @@ import LogoGrandMotors from 'images/logo-grand-motors.png';
 import LogoToyota from 'images/header/logo-toyota.png';
 
 import CarroEtiosHatch from 'images/header/etios-hatch.png';
+import CarroEtiosSeda from 'images/header/etios-seda.png';
+import CarroEtiosCross from 'images/header/etios-cross.png';
+import CarroCorolla from 'images/header/corolla.png';
+import CarroHilux from 'images/header/hilux.png';
 
 // informacoes sobre os telefones de contato
 const MockPhones = [
@@ -24,13 +28,16 @@ const MockPhones = [
 
 // simulacao de dados dos carros listados no menu "Nossos Veiculos"
 const MockCars = [
-  { name: 'Etios Sedã 2017', icon: CarroEtiosHatch },
-  { name: 'Etios Sedã 2017', icon: CarroEtiosHatch },
-  { name: 'Etios Sedã 2017', icon: CarroEtiosHatch },
-  { name: 'Etios Sedã 2017', icon: CarroEtiosHatch },
-  { name: 'Etios Sedã 2017', icon: CarroEtiosHatch },
-  { name: 'Etios Sedã 2017', icon: CarroEtiosHatch },
-  { name: 'Etios Sedã 2017', icon: CarroEtiosHatch },
+  { name: 'Etios Hatch 2017', icon: CarroEtiosHatch },
+  { name: 'Etios Sedã 2017', icon: CarroEtiosSeda },
+  { name: 'Etios Cross 2017', icon: CarroEtiosCross },
+  { name: 'Corolla 2016', icon: CarroCorolla },
+  { name: 'Hilux CD Diesel', icon: CarroHilux },
+  { name: 'Etios Hatch 2017', icon: CarroEtiosHatch },
+  { name: 'Etios Sedã 2017', icon: CarroEtiosSeda },
+  { name: 'Etios Cross 2017', icon: CarroEtiosCross },
+  { name: 'Corolla 2016', icon: CarroCorolla },
+  { name: 'Hilux CD Diesel', icon: CarroHilux },
 ];
 
 export {
